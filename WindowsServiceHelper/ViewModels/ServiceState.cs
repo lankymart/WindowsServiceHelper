7bc51d4be57aaa4c926d31b93a62862f391ac506
Anderson <Anderson@Anderson-PC>
2011-06-24 01:26:14 -0500

    Initial working version

---
+++ WindowsServiceHelper/ViewModels/ServiceState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ServiceProcess.Helpers.ViewModels
+{
+    internal enum ServiceState
+    {
+        Stopping,
+        Stopped,
+        Starting,
+        Started,
+        Pausing,
+        Paused
+    }
+}
---
+++ WindowsServiceHelper/ViewModels/ServiceViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI;
+using ReactiveUI.Xaml;
+using System.ServiceProcess;
+using ServiceProcess.Helpers;
+using System.Reactive.Linq;
+using System.Windows;
+using ServiceProcess.Helpers.Helpers;
+
+namespace ServiceProcess.Helpers.ViewModels
+{
+    internal class ServiceViewModel : ReactiveObject
+    {
+        public ReactiveCommand StartCommand { get; private set; }
+        public ReactiveCommand PauseCommand { get; private set; }
+        public ReactiveCommand StopCommand { get; private set; }
+        public ReactiveCommand ContinueCommand { get; private set; }
+
+
+        public string Name { get; private set; }
+
+        private ServiceState _CurrentState = ServiceState.Stopped;
+        public ServiceState CurrentState
+        {
+            get { return _CurrentState; }
+            set { this.RaiseAndSetIfChanged(x => x.CurrentState, value); }
+        }
+
+        public ServiceViewModel(ServiceBase service)
+        {
+            Name = service.ServiceName;
+
+            var currentStateObs = this.ObservableForProperty(x => x.CurrentState).Select(c => c.GetValue());
+
+            StartCommand = new ReactiveCommand(currentStateObs.Select(s => s == ServiceState.Stopped));
+            StopCommand = new ReactiveCommand(currentStateObs.Select(s => s == ServiceState.Started));
+            PauseCommand = new ReactiveCommand(currentStateObs.Select(s => s == ServiceState.Started && service.CanPauseAndContinue));
+            ContinueCommand = new ReactiveCommand(currentStateObs.Select(s => s == ServiceState.Paused && service.CanPauseAndContinue));
+
+            AssignmentSubscription(StartCommand, () => ServiceBaseHelpers.StartService(service));
+            AssignmentSubscription(StopCommand, () => ServiceBaseHelpers.StopService(service));
+            AssignmentSubscription(PauseCommand, () => ServiceBaseHelpers.PauseService(service));
+            AssignmentSubscription(ContinueCommand, () => ServiceBaseHelpers.ContinueService(service));
+
+        }
+
+        private void AssignmentSubscription(ReactiveCommand command, Func<IObservable<ServiceState>> serviceOperation)
+        {
+            command.SelectMany(_ => serviceOperation().SubscribeOnDispatcher())
+                    .Subscribe
+                    (
+                        s => CurrentState = s, 
+                        ex => MessageBox.Show(ex.ToString())
+                    );        
+        }
+
+    }
+}
