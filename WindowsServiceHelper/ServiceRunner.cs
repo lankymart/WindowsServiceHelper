880f4a5d85df1f3961267a91b044a948880b1ddc
Anderson Imes <anderson@theimes.com>
2011-06-24 01:26:14 -0500

    Initial working version

---
+++ WindowsServiceHelper/ServiceRunner.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+using System.Windows;
+using ServiceProcess.Helpers.ViewModels;
+using System.Diagnostics;
+using System.Threading;
+using System.Reactive.Concurrency;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Schedulers;
+
+namespace ServiceProcess.Helpers
+{
+    public static class ServiceRunner
+    {
+        
+
+        public static void LoadServices(this IEnumerable<ServiceBase> services)
+        {
+            if (Debugger.IsAttached)
+            {
+                var t = Task.Factory.StartNew(() =>
+                    {
+                        App app = new App();
+                        app.InitializeComponent();
+                        app.Startup += (o, e) =>
+                            {
+                                Window window = new Window();
+                                window.Content = new ServicesControllerViewModel(services);
+                                window.Show();
+                            };
+                        app.Run();
+                    }, CancellationToken.None, TaskCreationOptions.PreferFairness, new StaTaskScheduler(25));
+                t.Wait();
+            }
+            else
+            {
+                ServiceBase.Run(services.ToArray());
+            }
+        }
+    }
+}
---
+++ WindowsServiceHelper/ViewModels/ServicesControllerViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ReactiveUI;
+using System.ServiceProcess;
+using ReactiveUI.Xaml;
+using System.Reactive.Linq;
+
+namespace ServiceProcess.Helpers.ViewModels
+{
+    internal class ServicesControllerViewModel : ReactiveObject
+    {
+        public IEnumerable<ServiceViewModel> Services { get; private set; }
+
+        private ServiceViewModel _SelectedService = null;
+        public ServiceViewModel SelectedService
+        {
+            get { return _SelectedService; }
+            set { this.RaiseAndSetIfChanged(x => x.SelectedService, value); }
+        }
+
+        public ReactiveCommand StartCommand { get; private set; }
+        public ReactiveCommand PauseCommand { get; private set; }
+        public ReactiveCommand StopCommand { get; private set; }
+        public ReactiveCommand ContinueCommand { get; private set; }
+
+        public ServicesControllerViewModel(IEnumerable<ServiceBase> services)
+        {
+            Services = services.Select(s => new ServiceViewModel(s)).ToList();
+
+            var selectedServiceObs = this.ObservableForProperty(x => x.SelectedService).Value();
+
+            StartCommand = new ReactiveCommand(selectedServiceObs.SelectMany(s => s.StartCommand.CanExecuteObservable));
+            StopCommand = new ReactiveCommand(selectedServiceObs.SelectMany(s => s.StopCommand.CanExecuteObservable));
+            PauseCommand = new ReactiveCommand(selectedServiceObs.SelectMany(s => s.PauseCommand.CanExecuteObservable));
+            ContinueCommand = new ReactiveCommand(selectedServiceObs.SelectMany(s => s.ContinueCommand.CanExecuteObservable));
+
+            StartCommand.Subscribe(_ => SelectedService.StartCommand.Execute(null));
+            StopCommand.Subscribe(_ => SelectedService.StopCommand.Execute(null));
+            PauseCommand.Subscribe(_ => SelectedService.PauseCommand.Execute(null));
+            ContinueCommand.Subscribe(_ => SelectedService.ContinueCommand.Execute(null));
+        }
+        
+    }
+}
